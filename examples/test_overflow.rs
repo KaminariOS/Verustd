b139d64ea2d94bd3d9ae10d623ce50aca5837f54
kosumi <chankocyo@gmail.com>
2025-03-14 10:27:08 -0400

    Cast -1 to nat is NaN

--- examples/test_overflow.rs
+++ examples/test_overflow.rs
@@ -4,11 +4,17 @@ verus!{
     // fn over(x: usize) -> usize {
     //     x + 1
     // }
-    fn over1(x: usize) -> (res: usize) 
-    requires x < 1
-    ensures res as nat - 1 < 0
-    {
-        x
+    // fn over1(x: usize) -> (res: usize) 
+    // requires x < 1
+    // ensures res as nat - 1 < 0
+    // {
+    //     x
+    // }
+    proof fn over_nat() {
+        let a: usize = 0;
+        // assert(a - 1 < 5);
+        // casting -1 to nat is like NaN
+        assert(((a - 1) as nat) == 5); 
     }
 }
 
