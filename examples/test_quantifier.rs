096254b8d3aaa6a8d2df327191598334de396b88
kosumi <chankocyo@gmail.com>
2025-03-23 13:38:44 -0400

    Add quantifier example

---
+++ examples/test_quantifier.rs
@@ -0,0 +1,18 @@
+use vstd::prelude::*;
+verus!{
+    spec fn a(i: nat) -> nat;
+    spec fn b(i: nat) -> nat;
+    spec fn c(i: nat, j: nat) -> bool;
+    spec fn d(i: nat) -> nat;
+    spec fn p(i: nat) -> nat;
+    proof fn testq(len: nat) {
+        assume(forall|i: nat| 0 <= i < len ==> #[trigger] c(a(i), a(p(i))) && a(p(i)) == b(p(i)) && a(i) == b(i));
+        assert(forall|i: nat| 0 <= i < len ==>  #[trigger] c(b(i), b(p(i))));
+    }
+    fn test_p(len: usize) {
+        proof {
+            assume(forall|i: nat| 0 <= i < len ==> #[trigger] c(a(i), a(p(i))) && a(p(i)) == b(p(i)) && a(i) == b(i));
+            assert(forall|i: nat| 0 <= i < len ==>  #[trigger] c(b(i), b(p(i))));
+        }
+    }
+}
