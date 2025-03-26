89048e43702783ffe5a12d635f3f417a2816489b
kosumi <chankocyo@gmail.com>
2025-03-26 16:32:37 -0400

    Token system

--- examples/test_tokens.rs
+++ examples/test_tokens.rs
@@ -31,16 +31,16 @@ impl MapWrapper {
             inner_map: Map::tracked_empty()
         }
     }
-//     proof fn allocate(tracked &mut self)
-//             requires old(self).well_formed()
-//             ensures old(self)@.len() + 1 == self@.len(), 
-// // self.well_formed() 
-//         {
-//         let len = self@.len();
-//         assert(!self@.dom().contains(len));
-//         self.inner_map.tracked_insert(len, WriteToken::fresh());
-//         assert(len == self@.len() + 1);
-//     }
+    proof fn allocate(tracked &mut self)
+            // requires old(self).well_formed()
+            // ensures old(self)@.len() + 1 == self@.len(), 
+// self.well_formed() 
+        {
+        // let len = self@.len();
+        // assert(!self@.dom().contains(len));
+        // self.inner_map.tracked_insert(len, WriteToken::fresh());
+        // assert(len == self@.len() + 1);
+    }
 
     fn deallocate(&mut self) {
     }
@@ -62,7 +62,6 @@ impl MapWrapper {
     }
 }
 
-// It seems that a Seq wrapper is better
 
 tracked struct WriteToken {
     keep: KeepToken,
@@ -112,6 +111,56 @@ impl WriteToken {
     }
 }
 
+    impl View for S {
+        type V = Map<nat, WriteToken>;
+        closed spec fn view(&self) -> Self::V {
+            self.map@
+        }
+    }
+    
+    struct S {
+        v: Vec<usize>,
+        map: Tracked<Map<nat, WriteToken>>        
+    }
+
+    impl S {
+        fn set(&mut self, i: usize, t: usize) 
+        requires i < old(self)@.len() && old(self).can_write(i as _)
+        {
+            
+        }
+
+        spec fn can_write(&self, i : nat) 
+        -> bool 
+        requires 
+        {
+            self@[i].well_formed()
+        }
+
+        spec fn well_formed(&self) -> bool {
+            &&& (forall|i: nat| 0 <= i < self@.len() ==> self@.dom().contains(i))
+            &&& (forall|i: nat|  i >= self@.len() ==> !self@.dom().contains(i))
+        }
+        spec fn well_formed_and_complete(&self) -> bool {
+            self.well_formed() && (forall|i: nat| 0 <= i < self@.len() ==> self@.dom().contains(i) && #[trigger] self@[i].well_formed())
+        }
+    }
+
+    struct Hole {
+        g: Tracked<GiveoutToken>
+    }
+    impl Hole {
+        fn new(tracked g: GiveoutToken) -> Self {
+            Self {
+                g: Tracked(g)
+            }
+        }
+    }
+    fn dropHole(h: Hole) -> (res: Tracked<GiveoutToken>) {
+        let Hole {g } = h;
+        g
+    }
+
     proof fn drop(tracked g: GiveoutToken) {}
     proof fn test_tokens() {
         let tracked mut t1 = WriteToken::fresh();
@@ -119,7 +168,7 @@ impl WriteToken {
         let tracked g1 = t1.split();
         let tracked g2 = t2.split();
         drop(g1);
-        t1.merge(g1);
+        // t1.merge(g1);
         // t1.merge(g2);
     }
     
