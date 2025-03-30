b1a505dd16647a1d941a6d92d6d170ef8d68e0f2
kosumi <chankocyo@gmail.com>
2025-03-30 13:34:43 -0400

    Add examples

--- examples/test_unwind.rs
+++ examples/test_unwind.rs
@@ -1,30 +1,40 @@
 use vstd::prelude::*;
 verus!{
-    // fn overflow() 
-    // -> u8
-    // no_unwind
-    // {
-    //     let i: u8 = 255;
-    //     let mut j = 0;
-    //     for k in 0..i 
-    //         invariant 
-    //             k <= u8::max,
-    //             j <= u8::max,
-    //     {
-    //         j =  k;
-    //     }
-    //     j
-    // }
+    fn func_with_a_loop() 
+    -> usize
+    no_unwind
+    {
+        let i: usize = 255;
+        let mut j = 0;
+        let mut k1 = 0;
+        while k1 < i
+            invariant 
+        
+        decreases i - k1
+        {
+            k1 += 1;
+            j =  k1;
+        }
+        j
+    }
 
-    // fn overflow_1(k: u8) 
-    // -> u8
-    // requires k as nat + 5 <= u8::MAX
-    // no_unwind
-    // {
-    //     k + 5
-    // }
+    fn div(k: u8, j: u8) 
+    -> u8
+    requires k as nat + 5 <= u8::MAX, j != 0
+    no_unwind
+    {
+        k / j
+    }
+
+    fn overflow_1(k: u8) 
+    -> u8
+    requires k as nat + 5 <= u8::MAX
+    no_unwind
+    {
+        k + 5
+    }
     
-    // fn overflow_1<T>(k: Option<T>) 
+    // fn unwrap<T>(k: Option<T>) 
     // -> T
     // requires k.is_some()
     // no_unwind
